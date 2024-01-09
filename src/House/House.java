731da649d24e9574434435c1f163631bc18ecea9
AshkanTheBoy <bedrok@bk.ru>
2024-01-09 17:24:24 +0300

    Homework

--- src/House/House.java
+++ src/House/House.java
@@ -1,20 +1,12 @@
 package House;
 
 public class House {
+
     private Material material = Material.WOOD;
     private int floors = 1;
     private int rooms = 1;
 
     public House(){}
-
-    @Override
-    public String toString() {
-        return "House{" +
-                "material=" + material +
-                ", floors=" + floors +
-                ", rooms=" + rooms +
-                '}';
-    }
 
     public House(Material material,int floors, int rooms){
         if (floors>3){
@@ -62,6 +54,14 @@ public class House {
         this.rooms = rooms;
     }
 
+    @Override
+    public String toString() {
+        return "House{" +
+                "material=" + material +
+                ", floors=" + floors +
+                ", rooms=" + rooms +
+                '}';
+    }
 
 }
 
